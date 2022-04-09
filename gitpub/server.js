71208e7d8a0cce508a669d96fa8c68f9172623da
Fernando Galvan <n.brighter00@gmail.com>
2022-04-09 17:02:50 -0500

    have my json file viewed

--- gitpub/server.js
+++ gitpub/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
+const database = require('./models/drinks')
 
 app.get('/', (req, res) => {
     res.send("You are on home server")
 })
 
-app.get("/drinks", (req, res) => {
-    
+app.get("/drinks/", (req, res) => {
+    res.send(database)
 })
 
 
 app.listen(PORT, (req, res) => {
-    console.log("Hello!")
+    console.log("Welcome to the Github App")
 })
