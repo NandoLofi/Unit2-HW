f27872158a8fc1ffca95f361e4ae9f747219920e
Fernando Galvan <n.brighter00@gmail.com>
2022-04-10 16:54:35 -0500

    added all functionality minus image rendering

--- gitpub/server.js
+++ gitpub/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
 const drinks = require('./models/drinks')
-const Jimp = require("jimp");
+const food = require('./models/food')
 
 app.get('/', (req, res) => {
     res.send("You are on home server")
@@ -11,13 +11,16 @@ app.get('/', (req, res) => {
 app.get("/drinks", (req, res) => {
     res.render('drinks_index.ejs', {drinks: drinks})
 })
-
 //Show Route
 app.get('/drinks/:id', (req, res) => {
     res.render('drinks_show.ejs', {drinks: drinks[req.params.id]})
 })
-
-
+app.get('/food', (req, res) => {
+    res.render('food_index.ejs', {food: food} )
+})
+app.get('/food/:id', (req, res) => {
+    res.render('food_show.ejs', {food: food[req.params.id]})
+})
 
 
 
