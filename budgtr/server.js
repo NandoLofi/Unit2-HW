7b1871b6496b33234342af3dfa2269b7f8a8a7cd
Fernando Galvan <n.brighter00@gmail.com>
2022-04-17 11:50:58 -0500

    basic budget page

--- budgtr/server.js
+++ budgtr/server.js
@@ -2,9 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const PORT = process.env.PORT
+const budget = require("./models/budget")
 
+
+//middleware
+app.use(express.urlencoded({extended: false}));
+app.use('/static', express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('You are home')
+})
+//index route
 app.get('/budgets', (req, res) => {
-    res.send()
+    res.render('index.ejs', budget)
 })
 app.get('/budgets/:index', (req, res) => {
     res.send()
